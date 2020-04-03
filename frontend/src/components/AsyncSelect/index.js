0f408dfefd1cdfe1d2dc2dce328c06a177811ff3
Dayvson Sales <dayvson@outlook.com>
2020-04-03 13:19:10 -0300

    Add AsyncSelect component based on unform

---
+++ frontend/src/components/AsyncSelect/index.js
@@ -0,0 +1,48 @@
+import React, { useRef, useEffect } from 'react';
+
+import { useField } from '@unform/core';
+import Select from 'react-select/async';
+
+export default function AsyncSelect({ name, loadOptions, ...rest }) {
+  const selectRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: selectRef.current,
+      path: 'select.state.value',
+      getValue: (ref) => {
+        if (rest.isMulti) {
+          if (!ref.select.state.value) {
+            return [];
+          }
+
+          return ref.select.state.value.map((option) => option.value);
+        }
+
+        if (!ref.select.state.value) {
+          return '';
+        }
+
+        return ref.select.state.value.value;
+      },
+    });
+  }, [fieldName, selectRef, registerField, rest.isMulti]);
+
+  return (
+    <>
+      <Select
+        ref={selectRef}
+        defaultValue={defaultValue}
+        cacheOptions
+        loadOptions={loadOptions}
+        components={{
+          IndicatorSeparator: () => null,
+        }}
+        {...rest}
+      />
+      {error && <span className="error">{error}</span>}
+    </>
+  );
+}
