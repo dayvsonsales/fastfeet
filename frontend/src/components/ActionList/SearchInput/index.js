8f4b59a059379b74ea6bbafc3dda93cd2a937477
Dayvson Sales <dayvson@outlook.com>
2020-04-02 22:25:08 -0300

    Hold enter key

--- frontend/src/components/ActionList/SearchInput/index.js
+++ frontend/src/components/ActionList/SearchInput/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -6,19 +6,36 @@ import { MdSearch } from 'react-icons/md';
 
 import { Container, Input } from './styles';
 
-export default function SearchInput({ placeholder }) {
+export default function SearchInput({ placeholder, onChange, onEnter }) {
+  const [value, setValue] = useState('');
+
+  document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13 || e.key === 'Enter') {
+      e.preventDefault();
+      onEnter(e.target.value);
+    }
+  });
+
   return (
     <Container>
       <MdSearch color="#999999" size={24} />
-      <Input placeholder={placeholder} />
+      <Input
+        value={value}
+        onChange={onChange || ((e) => setValue(e.target.value))}
+        placeholder={placeholder}
+      />
     </Container>
   );
 }
 
 SearchInput.propTypes = {
   placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  onEnter: PropTypes.func,
 };
 
 SearchInput.defaultProps = {
   placeholder: '',
+  onChange: null,
+  onEnter: null,
 };
