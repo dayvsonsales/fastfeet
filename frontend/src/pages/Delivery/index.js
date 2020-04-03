63b18184e9866722c9ed0dac3b489927256b295a
Dayvson Sales <dayvson@outlook.com>
2020-04-02 22:26:19 -0300

    Load deliveries and search by product

--- frontend/src/pages/Delivery/index.js
+++ frontend/src/pages/Delivery/index.js
@@ -31,16 +31,82 @@ import DeliveryStatus from '~/components/DeliveryStatus';
 import Modal from '~/components/Modal';
 import { Dropdown, DropdownContent } from '~/components/Dropdown/styles';
 
-import image from '~/assets/Image 1.png';
 import SearchInput from '~/components/ActionList/SearchInput';
 
 export default function Delivery({ history }) {
   const refModal = useRef(null);
   const [show, setShow] = useState(false);
+  const [deliveries, setDeliveries] = useState([]);
+  const [currentDelivery, setCurrentDelivery] = useState(null);
+  const [product, setProduct] = useState('');
+
+  function handleSearch(_product) {
+    console.tron.log(product);
+    setProduct(_product);
+  }
+
+  function handleModal(delivery) {
+    setCurrentDelivery(delivery);
+    setShow(true);
+  }
 
   function handleClickOutside() {
     setShow(false);
   }
+
+  function generateSlug(name) {
+    const split = name.split(' ');
+
+    let slug;
+
+    if (split.length > 1) {
+      slug = `${split[0].substring(0, 1)}${split[1].substring(0, 1)}`;
+    } else {
+      slug = `${split[0].substring(0, 1)}${split[0].substring(1, 2)}`;
+    }
+
+    return slug;
+  }
+
+  function type(delivery) {
+    if (delivery.canceled_at) {
+      return 'canceled';
+    }
+
+    if (delivery.end_date) {
+      return 'delivered';
+    }
+
+    if (delivery.start_date) {
+      return 'ready';
+    }
+
+    return 'pending';
+  }
+
+  async function loadDeliveries(page = 1) {
+    const response = await api.get(`/delivery?page=${page}&q=${product}`);
+
+    const data = response.data.rows.map((delivery) => {
+      const { name } = delivery.deliveryman;
+
+      delivery.deliveryman.slug = generateSlug(name);
+      delivery.type = type(delivery);
+
+      return {
+        ...delivery,
+        deliveryman: {
+          ...delivery.deliveryman,
+        },
+      };
+    });
+
+    setDeliveries(data);
+  }
+
+  useEffect(() => {
+    loadDeliveries();
+  }, [product]);
 
   return (
     <>
@@ -49,7 +115,10 @@ export default function Delivery({ history }) {
           <strong>Gerenciando encomendas</strong>
         </header>
         <ActionList>
-          <SearchInput placeholder="Buscar por encomendas" />
+          <SearchInput
+            placeholder="Buscar por encomendas"
+            onEnter={handleSearch}
+          />
           <button
             type="button"
             onClick={() => history.push('/delivery/create')}
@@ -72,96 +141,82 @@ export default function Delivery({ history }) {
           </thead>
 
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>JanuaryJanuary</td>
-              <td>
-                <Profile>
-                  <div>JD</div>
-                  John Doe
-                </Profile>
-              </td>
-              <td>$100</td>
-              <td>January</td>
-              <td>
-                <DeliveryStatus />
-              </td>
-              <td>
-                <Dropdown>
-                  <MdMoreHoriz size={24} color="#C6C6C6" />
-                  <DropdownContent>
-                    <a href="#d" onClick={() => setShow(true)}>
-                      <MdVisibility size={10} color="#8E5BE8" />
-                      Visualizar
-                    </a>
-                    <Link to="/edit">
-                      <MdCreate size={10} color="#4D85EE" />
-                      Editar
-                    </Link>
-                    <Link to="/delete">
-                      <MdDeleteForever size={10} color="#DE3B3B" />
-                      Excluir
-                    </Link>
-                  </DropdownContent>
-                </Dropdown>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>JanuaryJanuary</td>
-              <td>
-                <Profile>
-                  <img src={image} alt="Assinatura" />
-                  John Doe
-                </Profile>
-              </td>
-              <td>$100</td>
-              <td>January</td>
-              <td>
-                <DeliveryStatus />
-              </td>
-              <td>
-                <Dropdown>
-                  <MdMoreHoriz size={24} color="#C6C6C6" />
-                  <DropdownContent>
-                    <a href="#ss">
-                      <MdVisibility size={10} color="#8E5BE8" />
-                      Visualizar
-                    </a>
-                    <Link to="/edit">
-                      <MdCreate size={10} color="#4D85EE" />
-                      Editar
-                    </Link>
-                    <Link to="/delete">
-                      <MdDeleteForever size={10} color="#DE3B3B" />
-                      Excluir
-                    </Link>
-                  </DropdownContent>
-                </Dropdown>
-              </td>
-            </tr>
+            {deliveries.map((delivery) => (
+              <tr key={delivery.id}>
+                <td>#{delivery.id < 10 ? `0${delivery.id}` : delivery.id}</td>
+                <td>{delivery.recipient.name}</td>
+                <td>
+                  <Profile>
+                    {delivery.deliveryman.avatar ? (
+                      <img
+                        src={delivery.deliveryman.avatar.url}
+                        alt={delivery.deliveryman.name}
+                      />
+                    ) : (
+                      <div>{delivery.deliveryman.slug}</div>
+                    )}
+                    {delivery.deliveryman.name}
+                  </Profile>
+                </td>
+                <td>{delivery.recipient.city}</td>
+                <td>{delivery.recipient.state}</td>
+                <td>
+                  <DeliveryStatus type={delivery.type} />
+                </td>
+                <td>
+                  <Dropdown>
+                    <MdMoreHoriz size={24} color="#C6C6C6" />
+                    <DropdownContent>
+                      <a href="#modal" onClick={() => handleModal(delivery)}>
+                        <MdVisibility size={10} color="#8E5BE8" />
+                        Visualizar
+                      </a>
+                      <Link to="/edit">
+                        <MdCreate size={10} color="#4D85EE" />
+                        Editar
+                      </Link>
+                      <Link to="/delete">
+                        <MdDeleteForever size={10} color="#DE3B3B" />
+                        Excluir
+                      </Link>
+                    </DropdownContent>
+                  </Dropdown>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
 
       <Modal ref={refModal} show={show} onClickOutside={handleClickOutside}>
-        <DeliveryInformation>
-          <strong>Informações da encomenda</strong>
-          <Street>Rua Ddadad </Street>
-          <CityAndState>Maceió - AL</CityAndState>
-          <Zip>57060-160</Zip>
-          <HorizontalLine />
-          <strong>Datas</strong>
-          <Date>
-            <strong>Retirada:</strong>24/05/2010
-          </Date>
-          <Date>
-            <strong>Entrega:</strong>27/10/1010
-          </Date>
-          <HorizontalLine />
-          <strong>Assinatura do destinatário</strong>
-          <img src={image} alt="Assinatura" />
-        </DeliveryInformation>
+        {currentDelivery && (
+          <DeliveryInformation>
+            <strong>Informações da encomenda</strong>
+            <Street>{currentDelivery.recipient.street}</Street>
+            <CityAndState>
+              {currentDelivery.recipient.city} -{' '}
+              {currentDelivery.recipient.state}
+            </CityAndState>
+            <Zip>{currentDelivery.recipient.zip_code}</Zip>
+            <HorizontalLine />
+            <strong>Datas</strong>
+            <Date>
+              <strong>Retirada:</strong>
+              {currentDelivery.start_date || 'Não iniciada!'}
+            </Date>
+            <Date>
+              <strong>Entrega:</strong>
+              {currentDelivery.end_date || 'Não finalizada!'}
+            </Date>
+            <HorizontalLine />
+            <strong>Assinatura do destinatário</strong>
+            {currentDelivery.signature ? (
+              <img src={currentDelivery.signature.url} alt="Assinatura" />
+            ) : (
+              <p>Sem imagem</p>
+            )}
+          </DeliveryInformation>
+        )}
       </Modal>
     </>
   );
