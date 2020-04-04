bde5a8bc8c3b6908d95ed32cac5047427cf3a30b
Dayvson Sales <dayvson@outlook.com>
2020-04-04 00:05:41 -0300

    List recipients

--- frontend/src/pages/Recipient/index.js
+++ frontend/src/pages/Recipient/index.js
@@ -4,6 +4,7 @@ import { MdAdd, MdMoreHoriz, MdDeleteForever, MdCreate } from 'react-icons/md';
 
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 
 import { Container } from './styles';
@@ -14,8 +15,51 @@ import { Dropdown, DropdownContent } from '~/components/Dropdown/styles';
 import SearchInput from '~/components/ActionList/SearchInput';
 import ActionList from '~/components/ActionList';
 import Table from '~/components/Table';
+import { generateSlug } from '~/utils/helper';
 
 export default function Recipient({ history }) {
+  const [recipients, setRecipients] = useState([]);
+  const [name, setName] = useState('');
+
+  function handleSearch(_name) {
+    setName(_name);
+  }
+
+  async function handleDelete({ id }) {
+    if (
+      window.confirm('Se você remover, não poderá mais recuperar. Tem certeza?')
+    ) {
+      try {
+        await api.delete(`recipients/${id}`);
+
+        setRecipients(recipients.filter((d) => d.id !== id));
+
+        toast.success('Removido com sucesso!');
+      } catch (e) {
+        toast.error(
+          'Ocorreu um erro ao remover entregador. Tente novamente mais tarde'
+        );
+      }
+    }
+  }
+
+  async function loadDeliverymen(page = 1) {
+    const response = await api.get(`/recipients?page=${page}&q=${name}`);
+
+    const data = response.data.rows.map((recipient) => ({
+      ...recipient,
+      full_address: `${recipient.street}, ${recipient.number || 'S/N'}, ${
+        recipient.city
+      } - ${recipient.state}`,
+    }));
+
+    setRecipients(data);
+  }
+
+  useEffect(() => {
+    loadDeliverymen();
+  }, [name]);
+
   return (
     <Container>
       <header>
@@ -23,7 +67,10 @@ export default function Recipient({ history }) {
       </header>
 
       <ActionList>
-        <SearchInput placeholder="Buscar por destinatários" />
+        <SearchInput
+          placeholder="Buscar por destinatários"
+          onEnter={handleSearch}
+        />
 
         <button type="button" onClick={() => history.push('/recipient/create')}>
           <MdAdd size={24} color="#fff" />
@@ -34,36 +81,36 @@ export default function Recipient({ history }) {
       <Table>
         <thead>
           <tr>
-            <th width="5%">ID</th>
+            <th width="8%">ID</th>
             <th width="20%">Nome</th>
-            <th width="20%">Endereço</th>
-            <th width="7%">Ações</th>
+            <th width="60%">Endereço</th>
+            <th width="5%">Ações</th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>JanuaryJanuary</td>
-            <td>
-              <DeliveryStatus />
-            </td>
-            <td>
-              <Dropdown>
-                <MdMoreHoriz size={24} color="#C6C6C6" />
-                <DropdownContent>
-                  <Link to="/edit">
-                    <MdCreate size={10} color="#4D85EE" />
-                    Editar
-                  </Link>
-                  <Link to="/delete">
-                    <MdDeleteForever size={10} color="#DE3B3B" />
-                    Excluir
-                  </Link>
-                </DropdownContent>
-              </Dropdown>
-            </td>
-          </tr>
+          {recipients.map((recipient) => (
+            <tr>
+              <td>#{recipient.id < 10 ? `0${recipient.id}` : recipient.id}</td>
+              <td>{recipient.name}</td>
+              <td>{recipient.full_address}</td>
+              <td>
+                <Dropdown>
+                  <MdMoreHoriz size={24} color="#C6C6C6" />
+                  <DropdownContent>
+                    <Link to={`/recipient/edit/${recipient.id}`}>
+                      <MdCreate size={10} color="#4D85EE" />
+                      Editar
+                    </Link>
+                    <Link href="delete" onClick={() => handleDelete(recipient)}>
+                      <MdDeleteForever size={10} color="#DE3B3B" />
+                      Excluir
+                    </Link>
+                  </DropdownContent>
+                </Dropdown>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </Container>
