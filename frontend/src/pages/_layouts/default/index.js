f8590783acd0586353511d28b442b6542afe50f4
Dayvson Sales <dayvson@outlook.com>
2020-04-04 20:21:23 -0300

    Fix proptypes

--- frontend/src/pages/_layouts/default/index.js
+++ frontend/src/pages/_layouts/default/index.js
@@ -14,5 +14,5 @@ export default function DefaultLayout({ children }) {
 }
 
 DefaultLayout.propTypes = {
-  children: PropTypes.instanceOf(Array).isRequired,
+  children: PropTypes.instanceOf(Object).isRequired,
 };
