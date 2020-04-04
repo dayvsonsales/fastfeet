5a723a82ecebb01472a531541bb8cdf7a46671dd
Dayvson Sales <dayvson@outlook.com>
2020-04-04 00:08:09 -0300

    List problem page

--- frontend/src/pages/Problem/index.js
+++ frontend/src/pages/Problem/index.js
@@ -2,12 +2,10 @@ import React, { useState, useRef, useEffect } from 'react';
 
 import { MdMoreHoriz, MdDeleteForever, MdCreate } from 'react-icons/md';
 
-import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 
-import image from '~/assets/Image 1.png';
-
-import { Container } from './styles';
+import { Container, ProblemInformation } from './styles';
 
 import { Dropdown, DropdownContent } from '~/components/Dropdown/styles';
 
@@ -18,9 +16,47 @@ export default function Problem() {
   const refModal = useRef(null);
   const [show, setShow] = useState(false);
 
+  const [deliveriesProblems, setDeliveryProblems] = useState([]);
+  const [currentProblem, setCurrentProblem] = useState();
+
+  function handleModal(deliveryProblem) {
+    setCurrentProblem(deliveryProblem);
+    setShow(true);
+  }
+
   function handleClickOutside() {
     setShow(false);
   }
+
+  async function handleCancel({ id }) {
+    if (
+      window.confirm(
+        'Se você cancelar, não poderá mais recuperar. Tem certeza?'
+      )
+    ) {
+      try {
+        await api.delete(`/problem/${id}/cancel-delivery`);
+
+        setDeliveryProblems(deliveriesProblems.filter((d) => d.id !== id));
+
+        toast.success('Removido com sucesso!');
+      } catch (e) {
+        toast.error(
+          'Ocorreu um erro ao remover encomenda. Tente novamente mais tarde'
+        );
+      }
+    }
+  }
+
+  useEffect(() => {
+    async function loadDeliveryProblems() {
+      const response = await api.get('delivery-problems');
+
+      setDeliveryProblems(response.data.rows);
+    }
+
+    loadDeliveryProblems();
+  }, []);
 
   return (
     <>
@@ -31,45 +67,76 @@ export default function Problem() {
         <Table>
           <thead>
             <tr>
-              <th width="5%">Encomenda</th>
-              <th width="20%">Problema</th>
-              <th width="7%">Ações</th>
+              <th width="20%">Encomenda</th>
+              <th width="75%">Problema</th>
+              <th width="5%">Ações</th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>JanuaryJanuary</td>
-              <td>
-                <Dropdown>
-                  <MdMoreHoriz size={24} color="#C6C6C6" />
-                  <DropdownContent>
-                    <a href="#ss" onClick={() => setShow(true)}>
-                      <MdCreate size={10} color="#4D85EE" />
-                      Visualizar
-                    </a>
-                    <Link to="/delete">
-                      <MdDeleteForever size={10} color="#DE3B3B" />
-                      Excluir
-                    </Link>
-                  </DropdownContent>
-                </Dropdown>
-              </td>
-            </tr>
+            {deliveriesProblems.map((deliveryProblem) => (
+              <tr>
+                <td>
+                  #
+                  {deliveryProblem.delivery.id < 10
+                    ? `0${deliveryProblem.delivery.id}`
+                    : deliveryProblem.delivery.id}
+                </td>
+                <td
+                  style={{
+                    maxWidth: '150px',
+                    textOverflow: 'ellipsis',
+                    overflow: 'hidden',
+                    whiteSpace: 'nowrap',
+                  }}
+                >
+                  {deliveryProblem.description}
+                </td>
+                <td>
+                  <Dropdown>
+                    <MdMoreHoriz size={24} color="#C6C6C6" />
+                    <DropdownContent>
+                      <a
+                        href="#problem"
+                        onClick={() => handleModal(deliveryProblem)}
+                      >
+                        <MdCreate size={10} color="#4D85EE" />
+                        Visualizar
+                      </a>
+                      <a
+                        href="#delete"
+                        onClick={() => handleCancel(deliveryProblem)}
+                      >
+                        <MdDeleteForever size={10} color="#DE3B3B" />
+                        Cancelar encomenda
+                      </a>
+                    </DropdownContent>
+                  </Dropdown>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
 
       <Modal ref={refModal} show={show} onClickOutside={handleClickOutside}>
-        <strong>VISUALIZAR PROBLEMA</strong>
-        Cillum sit reprehenderit est exercitation.Enim laboris ipsum quis Lorem
-        pariatur. Non in id do voluptate in magna voluptate magna ut quis
-        commodo aliquip. Magna adipisicing qui elit Lorem sit sint labore et
-        tempor aliquip cupidatat veniam. Enim officia deserunt ut consectetur
-        occaecat anim qui aliquip ipsum. Ut veniam culpa laboris veniam sit.
-        Pariatur do dolore excepteur id. Ipsum ad amet officia adipisicing
-        incididunt laborum commodo ut occaecat eiusmod anim occaecat quis.
+        {currentProblem && (
+          <ProblemInformation>
+            <strong>VISUALIZAR PROBLEMA</strong>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in
+              mauris et felis eleifend elementum vel quis lectus. Vivamus
+              dapibus nisi augue, vitae ultrices ligula elementum at. Proin ut
+              metus in mi tincidunt vestibulum a a felis. Aenean dictum libero
+              eu urna tristique vestibulum. Fusce feugiat justo et augue
+              facilisis, sit amet ornare eros consequat. Suspendisse semper
+              risus feugiat nisl commodo, sed mollis neque auctor. Nullam eu
+              fringilla lectus. Phasellus sed sapien sed turpis imperdiet
+              maximus. Aenean ante nulla, bibendum non facilisis at, facilisis
+              eget ex. In ut quam et tellus aliquet tincidunt.
+            </span>
+          </ProblemInformation>
+        )}
       </Modal>
     </>
   );
--- frontend/src/pages/Problem/styles.js
+++ frontend/src/pages/Problem/styles.js
@@ -20,3 +20,23 @@ export const Container = styled.div`
     }
   }
 `;
+
+export const ProblemInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 60px;
+
+  strong {
+    color: #444444;
+  }
+
+  span {
+    margin-top: 5px;
+    text-align: left;
+    letter-spacing: 0px;
+    color: #666666;
+    font-size: 16px;
+    line-height: 1.6;
+  }
+`;
