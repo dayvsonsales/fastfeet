2985f92881eea923b2ced3933fd7cc674c5089e1
Dayvson Sales <dayvson@outlook.com>
2020-04-04 20:23:19 -0300

    Add schema validation

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form } from '@unform/web';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 
+import { toast } from 'react-toastify';
 import logo from '../../assets/logo.png';
 import { signInRequest } from '../../store/modules/auth/action';
 import Input from '~/components/Input';
@@ -15,17 +16,42 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
+  const formRef = useRef(null);
   const loading = useSelector((state) => state.auth.loading);
   const dispatch = useDispatch();
 
-  function handleSubmit({ email, password }) {
-    dispatch(signInRequest(email, password));
+  async function handleSubmit(data) {
+    const { email, password } = data;
+    try {
+      formRef.current.setErrors({});
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      dispatch(signInRequest(email, password));
+    } catch (err) {
+      console.tron.log(err);
+      const validationErrors = {};
+
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(validationErrors);
+      } else {
+        toast.error(
+          'Não foi possível realizar login, tente novamente mais tarde!'
+        );
+      }
+    }
   }
 
   return (
     <>
-      <img src={logo} alt="GoBarber" />
-      <Form schema={schema} onSubmit={handleSubmit}>
+      <img src={logo} alt="Fastfeet" />
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <label htmlFor="email">Seu e-mail</label>
         <Input
           id="email"
