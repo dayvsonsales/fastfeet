472ab2e61b182048ea732b9192a93257e7e80e3d
Dayvson Sales <dayvson@outlook.com>
2020-04-04 00:06:55 -0300

    Separate slug and type helpers method

---
+++ frontend/src/utils/helper.js
@@ -0,0 +1,29 @@
+export function generateSlug(name) {
+  const split = name.split(' ');
+
+  let slug;
+
+  if (split.length > 1) {
+    slug = `${split[0].substring(0, 1)}${split[1].substring(0, 1)}`;
+  } else {
+    slug = `${split[0].substring(0, 1)}${split[0].substring(1, 2)}`;
+  }
+
+  return slug;
+}
+
+export function type(delivery) {
+  if (delivery.canceled_at) {
+    return 'canceled';
+  }
+
+  if (delivery.end_date) {
+    return 'delivered';
+  }
+
+  if (delivery.start_date) {
+    return 'ready';
+  }
+
+  return 'pending';
+}
