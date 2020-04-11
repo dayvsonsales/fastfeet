3867ba1973d7cb029b2d2c4af88ad60d31cf894d
Dayvson Sales <dayvson@outlook.com>
2020-04-11 14:12:33 -0300

    Remove unused user saga methods

--- frontend/src/store/modules/auth/reducer.js
+++ frontend/src/store/modules/auth/reducer.js
@@ -7,7 +7,7 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  return produce(state, draft => {
+  return produce(state, (draft) => {
     switch (action.type) {
       case '@auth/SIGN_IN_REQUEST': {
         draft.loading = true;
@@ -24,7 +24,6 @@ export default function auth(state = INITIAL_STATE, action) {
         break;
       }
       default:
-        return state;
     }
   });
 }
