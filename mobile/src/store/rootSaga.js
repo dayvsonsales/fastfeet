d0b40a8d6a406bb8b5f22a0658a8cbd1c6b0a092
Dayvson Sales <dayvson@outlook.com>
2020-04-11 00:19:56 -0300

    Minor refactor

--- mobile/src/store/rootSaga.js
+++ mobile/src/store/rootSaga.js
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
 import auth from './modules/auth/sagas';
-import user from './modules/user/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user]);
+  return yield all([auth]);
 }
