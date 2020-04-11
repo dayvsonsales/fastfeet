0a2a662e6df39fc29adbd6682170587c835b931c
Dayvson Sales <dayvson@outlook.com>
2020-04-11 00:10:29 -0300

    Load recipients

--- backend/src/app/controllers/EndedDeliveryController.js
+++ backend/src/app/controllers/EndedDeliveryController.js
@@ -1,6 +1,7 @@
 import { Op } from 'sequelize';
 import Delivery from '../models/Delivery';
 import Deliveryman from '../models/Deliveryman';
+import Recipient from '../models/Recipient';
 
 class EndedDeliveryController {
   async index(req, res) {
@@ -17,6 +18,13 @@ class EndedDeliveryController {
           [Op.ne]: null,
         },
       },
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: ['id', 'city'],
+        },
+      ],
     });
 
     return res.json(deliveries);
